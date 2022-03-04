dfa2d33fc3c2611d427480e077ed928bd8dde89e
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2022-03-04 19:00:17 +0100

    added more test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -6,8 +6,7 @@ import org.mockito.internal.matchers.GreaterThan;
 import org.mockito.internal.matchers.LessThan;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
@@ -39,5 +38,10 @@ public class TestGreeter {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
+  @Test
+  public void someSample() {
+    assertThat(15, isA(Integer.class));
   }
 }
